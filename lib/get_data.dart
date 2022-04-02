283736ed0d0a2d15b58226310b6371f94e7ad2f3
Luke Vo <lukevo1101@gmail.com>
2022-04-02 11:51:39 +0700

    Refactor Get data with Stream builder

--- lib/get_data.dart
+++ lib/get_data.dart
@@ -1,43 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
-class GetData extends StatelessWidget {
+class GetData {
   final String? imageUrl;
-  final String? quotes;
-  final String? documentId;
-  final TextStyle? style;
-  final TextAlign? textAlign;
-  // Create a list
-  List<String> maindata = [];
+  final String? quote;
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  GetData({this.imageUrl, this.quotes, this.documentId, this.style, this.textAlign});
-
-  @override
-  Widget build(BuildContext context) {
-    CollectionReference images =
-        FirebaseFirestore.instance.collection('maindata');
-
-    return FutureBuilder<DocumentSnapshot>(
-      future: images.doc(documentId).get(),
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong ");
-        }
-
-        if (snapshot.hasData && !snapshot.data!.exists) {
-          return Text("Document does not exist");
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
-          return Text("imageUrl: ${data['imageUrl']} and Quote: ${data['quote']}", style: style, textAlign: textAlign,);
-        }
-
-        return Text("loading");
-      },
-    );
-  }
+  GetData(
+      {this.imageUrl, this.quote});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:motivational_quote_app/constants.dart';
@@ -8,7 +9,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: "AIzaSyDzb7PDPmw54KdsYhT15sVHiBqGVoNBjE0", projectId: 'motivational-quote-app', messagingSenderId: '377603465409', appId: '1:377603465409:android:1800504cc8c11e23373440', // Your apiKey
+      apiKey: "AIzaSyDzb7PDPmw54KdsYhT15sVHiBqGVoNBjE0",
+      projectId: 'motivational-quote-app',
+      messagingSenderId: '377603465409',
+      appId: '1:377603465409:android:1800504cc8c11e23373440', // Your apiKey
     ),
   );
   runApp(const MyApp());
@@ -40,9 +44,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late int currentIndexNumber = 0;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   List<String> images = [
-
     "https://scontent.fsgn5-8.fna.fbcdn.net/v/t1.6435-9/191712776_1132407380588345_3179710775824733340_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=825194&_nc_ohc=nvpnIFHTlI4AX-eSd0Y&_nc_ht=scontent.fsgn5-8.fna&oh=00_AT9pO022M9bhBBEebmkpk_4QF4H0sWoLCSUWNwr3FExEaA&oe=62643DD9",
     "https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
     "https://images.unsplash.com/photo-1537444399873-da0fea0cf4b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
@@ -60,64 +64,71 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: kBgColor,
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(top: 20, left: 5, right: 5, bottom: 10),
-          child: Column(children: [
-            Swiper(
-              outer: true,
-              onIndexChanged: (value) {
-                setState(() {
-                  currentIndexNumber = value;
-                });
-              },
-              curve: Curves.linear,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(kImageBorder),
-                    image: DecorationImage(
-                      image: NetworkImage(images[index]),
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                );
-              },
-              itemCount: images.length,
-              itemWidth: kImageWidth,
-              itemHeight: kImageHeight,
-              layout: SwiperLayout.STACK,
-            ),
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Quote of the Day',
-                    style: kTitleTextStyle,
-                  ),
-                  GetData(
-                    textAlign: TextAlign.center,
-                    style: kSubTitleTextStyle,
-                    documentId: "jPbFMLxf0o64UC0qxjsj",
-                  ),
-                  SizedBox(
-                    height: 55,
-                    width: 120,
-                    child: TextButton(
-                      style: kButtonStyle,
-                      onPressed: () {},
-                      child: const Text(
-                        'Like 👍',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+        child: StreamBuilder(
+          stream: firestore.collection('maindata').snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+
+            return Container(
+              padding: EdgeInsets.only(top: 20, left: 5, right: 5, bottom: 10),
+              child: Column(children: [
+                Swiper(
+                  outer: true,
+                  onIndexChanged: (value) {
+                    setState(() {
+                      currentIndexNumber = value;
+                    });
+                  },
+                  curve: Curves.linear,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(kImageBorder),
+                        image: DecorationImage(
+                          image: NetworkImage(snapshot.data!.docs[index]["imageUrl"]),
+                          fit: BoxFit.fitHeight,
+                        ),
                       ),
-                    ),
+                    );
+                  },
+                  itemCount: snapshot.data!.docs.length,
+                  itemWidth: kImageWidth,
+                  itemHeight: kImageHeight,
+                  layout: SwiperLayout.STACK,
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Quote of the Day',
+                        style: kTitleTextStyle,
+                      ),
+                      Text(
+                        'Do it now! Now or Never!!!',
+                        style: kSubTitleTextStyle,
+                      ),
+                      SizedBox(
+                        height: 55,
+                        width: 120,
+                        child: TextButton(
+                          style: kButtonStyle,
+                          //TODO: Add Like button logic
+                          onPressed: () {},
+                          child: const Text(
+                            'Like 👍',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ))
-          ]),
+                ))
+              ]),
+            );
+          },
         ),
       ),
     );
