c90425d8fdf204e9a0d8a6cf403ca716f90da5bc
Luke Vo <lukevo1101@gmail.com>
2022-04-03 03:04:38 +0700

    Add logic for like button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:motivational_quote_app/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:like_button/like_button.dart';
 import 'package:motivational_quote_app/get_data.dart';
 
 void main() async {
@@ -45,19 +46,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late int currentIndexNumber = 0;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  //
-  // List<String> images = [
-  //   "https://scontent.fsgn5-8.fna.fbcdn.net/v/t1.6435-9/191712776_1132407380588345_3179710775824733340_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=825194&_nc_ohc=nvpnIFHTlI4AX-eSd0Y&_nc_ht=scontent.fsgn5-8.fna&oh=00_AT9pO022M9bhBBEebmkpk_4QF4H0sWoLCSUWNwr3FExEaA&oe=62643DD9",
-  //   "https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80",
-  //   "https://images.unsplash.com/photo-1537444399873-da0fea0cf4b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
-  // ];
-  //
-  // List<String> quotes = [
-  //   "Do it for the people who want to see you fail.",
-  //   "The way to get started is to quit talking and begin doing.",
-  //   "The purpose of our lives is to be happy.",
-  //   "You only live once, but if you do it right, once is enough."
-  // ];
+  final size = 30.0;
+  bool isLiked = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -67,6 +58,8 @@ class _HomePageState extends State<HomePage> {
         child: StreamBuilder(
           stream: firestore.collection('maindata').snapshots(),
           builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            int likeCount = snapshot.data!.docs[currentIndexNumber]
+            ["like"];
             if (!snapshot.hasData) return const Text('Loading...');
 
             return Container(
@@ -81,6 +74,8 @@ class _HomePageState extends State<HomePage> {
                   },
                   curve: Curves.linear,
                   itemBuilder: (context, index) {
+                    var likeCount = snapshot.data!.docs[currentIndexNumber]
+                    ["like"];
                     return Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(kImageBorder),
@@ -118,9 +113,39 @@ class _HomePageState extends State<HomePage> {
                           style: kButtonStyle,
                           //TODO: Add Like button logic
                           onPressed: () {},
-                          child: const Text(
-                            'Like 👍',
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                          child: LikeButton(
+                            isLiked: isLiked,
+                            likeBuilder: (isLiked) {
+                              return Icon(isLiked? Icons.favorite: Icons.favorite_border_outlined, color: isLiked? Colors.red:Colors.grey,);
+                            },
+                            likeCountPadding: EdgeInsets.only(left: 5.0),
+                            size: 30,
+                            countPostion: CountPostion.left,
+                            likeCount: likeCount,
+                            onTap: (isLiked) async {
+                              if (isLiked) {
+                                firestore
+                                    .collection("maindata")
+                                    .doc(snapshot
+                                    .data!.docs[currentIndexNumber].id)
+                                    .update({'like': FieldValue.increment(-1)});
+                                setState(() {
+                                  likeCount -=1;
+                                  this.isLiked = false;
+                                });
+                              } else if (!isLiked) {
+                                firestore
+                                    .collection("maindata")
+                                    .doc(snapshot
+                                    .data!.docs[currentIndexNumber].id)
+                                    .update({'like': FieldValue.increment(1)});
+                                setState(() {
+                                  likeCount +=1;
+                                  this.isLiked = true;
+                                });
+                              }
+
+                            },
                           ),
                         ),
                       ),
@@ -135,3 +160,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+// const Text(
+// 'Like 👍',
+// style: TextStyle(fontWeight: FontWeight.bold),
+// )
