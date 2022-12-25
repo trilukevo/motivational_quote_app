e70f31de8a7fe0e0ad34392830cd0c41348f1233
Luke Vo <lukevo1101@gmail.com>
2022-12-25 17:58:16 +0700

    feature/share-quote

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:motivational_quote_app/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:like_button/like_button.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -44,20 +46,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-   int currentIndexNumber = 0;
+  int currentIndexNumber = 0;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   final size = 30.0;
 
   @override
   Widget build(BuildContext context) {
+    String image = '';
+    String quote = '';
+    int likeCount = 0;
+    bool isLoved = true;
     return Scaffold(
       backgroundColor: kBgColor,
       body: SafeArea(
         child: StreamBuilder(
           stream: firestore.collection('maindata').snapshots(),
           builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            int likeCount = snapshot.data!.docs[currentIndexNumber]["like"];
-            bool isLoved = snapshot.data!.docs[currentIndexNumber]["isLiked"] ?? true;
+            likeCount = snapshot.data!.docs[currentIndexNumber]["like"];
+            isLoved =
+                snapshot.data!.docs[currentIndexNumber]["isLiked"] ?? true;
             if (!snapshot.hasData) return const Text('Loading...');
 
             return Container(
@@ -72,8 +79,6 @@ class _HomePageState extends State<HomePage> {
                   },
                   curve: Curves.linear,
                   itemBuilder: (context, index) {
-                    // var likeCount =
-                    //     snapshot.data!.docs[currentIndexNumber]["like"];
                     return Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(kImageBorder),
@@ -105,59 +110,106 @@ class _HomePageState extends State<HomePage> {
                         textAlign: TextAlign.center,
                         style: kSubTitleTextStyle,
                       ),
-                      SizedBox(
-                        height: 55,
-                        width: 120,
-                        child: TextButton(
-                          style: kButtonStyle,
-                          //TODO: Add Like button logic
-                          onPressed: () {},
-                          child: LikeButton(
-                            isLiked: isLoved,
-                            likeBuilder: (isLiked) {
-                              return Icon(
-                                isLiked
-                                    ? Icons.favorite
-                                    : Icons.favorite_border_outlined,
-                                color: isLiked ? Colors.red : Colors.grey,
-                              );
-                            },
-                            likeCountPadding: EdgeInsets.only(left: 5.0),
-                            size: 30,
-                            countPostion: CountPostion.left,
-                            likeCount: likeCount,
-                            onTap: (isLiked) async {
-                              if (isLiked) {
-                                firestore
-                                    .collection("maindata")
-                                    .doc(snapshot
-                                        .data!.docs[currentIndexNumber].id)
-                                    .update({
-                                  'like': FieldValue.increment(-1),
-                                  'isLiked': false
-                                });
-                                setState(() {
-                                  likeCount -= 1;
-                                  isLoved = false;
-                                });
-                              } else if (!isLiked) {
-                                firestore
-                                    .collection("maindata")
-                                    .doc(snapshot
-                                        .data!.docs[currentIndexNumber].id)
-                                    .update({
-                                  'like': FieldValue.increment(1),
-                                  'isLiked': true
-                                });
-                                setState(() {
-                                  likeCount += 1;
-                                  isLoved = true;
-                                });
-                              }
-                              return null;
-                            },
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          //Like button
+                          SizedBox(
+                            height: 55,
+                            width: 120,
+                            child: TextButton(
+                              style: kButtonStyle,
+                              onPressed: () {},
+                              child: LikeButton(
+                                isLiked: isLoved,
+                                likeBuilder: (isLiked) {
+                                  return Icon(
+                                    isLiked
+                                        ? Icons.favorite
+                                        : Icons.favorite_border_outlined,
+                                    color: isLiked ? Colors.red : Colors.grey,
+                                  );
+                                },
+                                likeCountPadding: EdgeInsets.only(left: 5.0),
+                                size: 30,
+                                countPostion: CountPostion.left,
+                                likeCount: likeCount,
+                                onTap: (isLiked) async {
+                                  if (isLiked) {
+                                    firestore
+                                        .collection("maindata")
+                                        .doc(snapshot
+                                            .data!.docs[currentIndexNumber].id)
+                                        .update({
+                                      'like': FieldValue.increment(-1),
+                                      'isLiked': false
+                                    });
+                                    setState(() {
+                                      likeCount -= 1;
+                                      isLoved = false;
+                                    });
+                                  } else if (!isLiked) {
+                                    firestore
+                                        .collection("maindata")
+                                        .doc(snapshot
+                                            .data!.docs[currentIndexNumber].id)
+                                        .update({
+                                      'like': FieldValue.increment(1),
+                                      'isLiked': true
+                                    });
+                                    setState(() {
+                                      likeCount += 1;
+                                      isLoved = true;
+                                    });
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
                           ),
-                        ),
+                          SizedBox(width: 20),
+                          //Share button
+                          SizedBox(
+                            height: 55,
+                            width: 120,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Stack(alignment: AlignmentDirectional.center
+                                  ,children: [
+                                Positioned.fill(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      gradient: LinearGradient(
+                                        colors: <Color>[
+                                          Color(0xFFeeaeca),
+                                          Color(0xFF94bbe9),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Share.share(snapshot.data!
+                                            .docs[currentIndexNumber]["quote"]);
+                                      },
+                                      style: TextButton.styleFrom(
+                                          foregroundColor: Colors.white,
+                                          padding: const EdgeInsets.all(16.0),
+                                          textStyle: const TextStyle(fontSize: 20)),
+                                      child: Text("Share",),
+                                    ),
+                                    Icon(Icons.ios_share, color: Colors.white,),
+                                  ],
+                                ),
+                              ]),
+                            ),
+                          )
+                        ],
                       ),
                     ],
                   ),
